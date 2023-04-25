a2875da498ecd31f550aad1f9b3e4826c4754b7c
pepesk34 <pepevic.dalias@gmail.com>
2023-04-25 16:19:52 +0200

    cambio

---
+++ Circulo.py
@@ -0,0 +1,12 @@
+class Circulo:
+
+    _radio:float
+
+    def __init__(radio: float):
+        self._radio = radio
+        
+    def area(self):
+        pass
+
+    def circunferencia(self):
+        pass
