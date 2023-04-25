44a492195f1fdde5537161597833bb9e9a8f8406
pablocalo <pcl0006@alu.medac.es>
2023-04-25 16:51:00 +0200

    cambio de archivo

--- Circulo.py
+++ Circulo.py
@@ -1,12 +1,18 @@
+#Este es mi pull request a Pepe
+
+from cmath import pi
+
 class Circulo:
 
     _radio:float
 
-    def __init__(radio: float):
-        self._radio = radio
+    def __init__(self,radio: float):
+        self._radio=radio
         
     def area(self):
-        pass
+        area = self._radio**2 * pi
+        return area
 
     def circunferencia(self):
-        pass
+        circunferencia = self._radio*2*pi
+        return circunferencia
